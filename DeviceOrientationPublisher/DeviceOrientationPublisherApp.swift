d4418efed3bc484d495efc1e2936316f03784d3a
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-02-03 16:38:42 +0530

    Initial Commit

---
+++ DeviceOrientationPublisher/DeviceOrientationPublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceOrientationPublisherApp.swift
+//  DeviceOrientationPublisher
+//
+//  Created by Sajed Shaikh on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DeviceOrientationPublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
